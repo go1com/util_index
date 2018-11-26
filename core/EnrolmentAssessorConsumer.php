433ba29ff579b562a0ba6c9024fc5a3107fd2d40
phuongh <thanhphuongat@gmail.com>
2018-11-26 10:58:03 +0700

    Handle event LO_SAVE_ASSESSORS

--- core/EnrolmentAssessorConsumer.php
+++ core/EnrolmentAssessorConsumer.php
@@ -13,6 +13,7 @@ use go1\util\lo\LoTypes;
 use go1\util\queue\Queue;
 use ONGR\ElasticsearchDSL\Query\Compound\BoolQuery;
 use ONGR\ElasticsearchDSL\Query\TermLevel\TermQuery;
+use ONGR\ElasticsearchDSL\Query\TermLevel\IdsQuery;
 use stdClass;
 
 class EnrolmentAssessorConsumer implements ConsumerInterface
@@ -34,12 +35,20 @@ class EnrolmentAssessorConsumer implements ConsumerInterface
 
     public function aware(string $event): bool
     {
-        return in_array($event, [Queue::ENROLMENT_SAVE_ASSESSORS, self::RETRY_ROUTING_KEY]);
+        return in_array($event, [
+            Queue::LO_SAVE_ASSESSORS,
+            Queue::ENROLMENT_SAVE_ASSESSORS,
+            self::RETRY_ROUTING_KEY
+        ]);
     }
 
     public function consume(string $routingKey, stdClass $msg, stdClass $context = null): bool
     {
         switch ($routingKey) {
+            case Queue::LO_SAVE_ASSESSORS:
+                $this->onCourse($routingKey, $msg);
+                break;
+
             case Queue::ENROLMENT_SAVE_ASSESSORS:
                 $this->onEnrolmentAssessorUpdated($routingKey, $msg);
                 break;
@@ -52,6 +61,60 @@ class EnrolmentAssessorConsumer implements ConsumerInterface
         }
 
         return true;
+    }
+
+    private function onCourse(string $routingKey, stdClass $body)
+    {
+        $courseId = $body->id;
+        $assessors = LoHelper::assessorIds($this->go1, $courseId);
+        try {
+            $this->client->updateByQuery([
+                'index' => Schema::INDEX,
+                'type'  => Schema::O_LO,
+                'body'  => [
+                    'query'  => (new IdsQuery([$courseId]))->toArray(),
+                    'script' => [
+                        'inline' => implode(";", [
+                            "ctx._source.assessor = params.assessor",
+                            "ctx._source.assessors = params.assessors",
+                        ]),
+                        'params' => [
+                            'assessor'  => $this->fEnrolment->assessor($assessors),
+                            'assessors' => $assessors,
+                        ],
+                    ]
+                ],
+                'wait_for_completion' => true,
+                'conflicts'           => 'proceed',
+            ]);
+        } catch (ElasticsearchException $e) {
+            $this->history->write(Schema::O_LO, $courseId, $e->getCode(), $e->getMessage());
+        }
+
+        $assessors = (count($assessors) <= 1) ? $assessors : [];
+        $query = new BoolQuery();
+        $query->add(new TermQuery('metadata.has_assessor', 0), BoolQuery::MUST);
+        $query->add(new TermQuery('metadata.course_id', $courseId), BoolQuery::MUST);
+
+        $response = $this->client->updateByQuery([
+            'index'   => Schema::INDEX,
+            'type'    => Schema::O_ENROLMENT,
+            'body'    => [
+                'query'  => $query->toArray(),
+                'script' => [
+                    'inline' => "ctx._source.assessors = params.assessors;",
+                    'params' => ['assessors' => $assessors],
+                ]
+            ],
+            'refresh'             => true,
+            # When a course changed assessors
+            # There will be 2 messages published(ro.create then ro.delete).
+            # We need wait until each query completed to avoid conflicts.
+            'wait_for_completion' => true,
+            'conflicts'           => 'proceed',
+        ]);
+
+        $this->handleConflict($response, $routingKey, $body);
     }
 
     private function onEnrolmentAssessorUpdated(string $routingKey, stdClass $body)
